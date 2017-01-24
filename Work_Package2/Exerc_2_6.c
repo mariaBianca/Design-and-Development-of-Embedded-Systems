75f6732e8d8615e385ed8102aa06deee1605345c
isofill <filip.saver15@gmail.com>
2017-01-24 01:24:33 +0100

    wrong file, changed it

--- Work_Package2/Exerc_2_6.c
+++ Work_Package2/Exerc_2_6.c
@@ -1,45 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define MAX 10
+#define MAX 5
+
+int list[MAX];
+
+void initQue(int list[], int max)
+{
+	int i = 0;
+	for (i=0; i < max; i++)
+	{
+    	list[i] = -1;
+	}
+}
+
+int input(int list[], int number, int max)
+{
+	int toggle = 0;
+	
+	int i = 0;
+	for (i=0; i < max; i++)
+	{
+		if(list[i] == -1 && toggle == 0)
+		{
+    		list[i] = number;
+    		toggle = 1;
+    	}
+	}
+	
+	return toggle;
+}
+
+int output(int list[], int max)
+{
+	int res = 0;
+	if(list[0] != -1)
+	{
+		res = list[0];
+		
+		int i = 0;
+		for (i=0; i < max-1; i++)
+		{
+    		list[i] = list[i+1];
+		}
+		list[max-1] = -1;
+		
+		return res;
+	}
+	return res;
+}
 
 int main(int argc, char *argv[]) {
 	
-//	int int_array[10]; 
-//	int i = 0;
-//	
-//	int res=0;
-//	
-//	for (i=0; i < (sizeof(int_array) / sizeof(int_array[0])); i++)
-//	{
-//    	int_array[i] = -1;
-//		printf("%d\n", int_array[i]);
-//	}
-	printf("hello");
+	int list[MAX];
+	int input_res, output_res;
+	int num = 0;
 	
-//	int str1length = 0;
-//	int str2length = 0;
-//	
-//	//Getting the lengths of the strings
-//	str1length = strlen(argv[1]);
-//	str2length = strlen(argv[2]);
-//
-//	//Checking with strcmp()
-//	res = strcmp(string1, string2);
-//	
-//	//Using result from strcmp(), AND doing another check without it
-//	if(res < 0 && str1length < str2length)
-//	{
-//		printf("String 1 is shorter!");
-//	}
-//	else if(res > 0 && str1length > str2length) 
-//	{
-//		printf("String 2 is shorter!");
-//	}
-//	else 
-//	{
-//		printf("Both strings are equal in length!");
-//	}
+	initQue(list, MAX);
+	
+	int i = 0;
+	for (i=0; i < MAX; i++)
+	{
+		printf("%d\n", list[i]);
+	}
+	
+	printf("Enter a postive integer:\n");
+	scanf("%d", &num);	
+	input_res = input(list, num, MAX);
+	
+	i = 0;
+	for (i=0; i < MAX; i++)
+	{
+		printf("%d\n", list[i]);
+	}
+	
+	printf("Enter a postive integer:\n");
+	scanf("%d", &num);
+	input_res = input(list, num, MAX);
+	
+	i = 0;
+	for (i=0; i < MAX; i++)
+	{
+		printf("%d\n", list[i]);
+	}
+	
+	output_res = output(list, MAX);
+	printf("Output: %d\n", output_res);
+	
+	i = 0;
+	for (i=0; i < MAX; i++)
+	{
+		printf("%d\n", list[i]);
+	}
+	
+	output_res = output(list, MAX);
+	printf("Output: %d\n", output_res);
+	
+	i = 0;
+	for (i=0; i < MAX; i++)
+	{
+		printf("%d\n", list[i]);
+	}
+	
+	output_res = output(list, MAX);
+	printf("Output: %d\n", output_res);
+	
+	i = 0;
+	for (i=0; i < MAX; i++)
+	{
+		printf("%d\n", list[i]);
+	}
+	
+	
 	
 	return 0;
 }
