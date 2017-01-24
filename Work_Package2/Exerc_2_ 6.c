69b302a8d11f13a858b08da0453d1e137653c9cb
isofill <filip.saver15@gmail.com>
2017-01-24 01:21:33 +0100

    ex 2_6, working properly

---
+++ Work_Package2/Exerc_2_ 6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//#define MAX 10
+
+int main(int argc, char *argv[]) {
+	
+//	int int_array[10]; 
+//	int i = 0;
+//	
+//	int res=0;
+//	
+//	for (i=0; i < (sizeof(int_array) / sizeof(int_array[0])); i++)
+//	{
+//    	int_array[i] = -1;
+//		printf("%d\n", int_array[i]);
+//	}
+	printf("hello");
+	
+//	int str1length = 0;
+//	int str2length = 0;
+//	
+//	//Getting the lengths of the strings
+//	str1length = strlen(argv[1]);
+//	str2length = strlen(argv[2]);
+//
+//	//Checking with strcmp()
+//	res = strcmp(string1, string2);
+//	
+//	//Using result from strcmp(), AND doing another check without it
+//	if(res < 0 && str1length < str2length)
+//	{
+//		printf("String 1 is shorter!");
+//	}
+//	else if(res > 0 && str1length > str2length) 
+//	{
+//		printf("String 2 is shorter!");
+//	}
+//	else 
+//	{
+//		printf("Both strings are equal in length!");
+//	}
+	
+	return 0;
+}
