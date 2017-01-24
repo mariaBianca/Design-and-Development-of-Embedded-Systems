f794c88a727c26ab0b6a2df5ea0edeb97f1dc499
Ninosauleo <ninosauleo@gmail.com>
2017-01-24 16:38:43 +0100

    Fixed 2.2

--- Work_Package2/exerc_2_2.c
+++ Work_Package2/exerc_2_2.c
@@ -1,64 +1,130 @@
-
 /*
- 
- File name: exerc_x_y.c (or cpp)
- 
- Date: 2017-01-23
- 
- Group Number:  #nr 4
- 
- Members of students contributed:
- 
- Rafael Antonino Sauleo
- 
- Filip Isakovski
- 
- Maria-Bianca Cindroi
- 
- Demonstration code: [<Examen code> <xxxx>] PENDING
- 
- 
- Create an array of integers, array[MAX], and fill it with MAX no of random numbers. Let then the
- program print out the following:
- The value of the label array (address) is: xxxxxxxxxx
- First integer in the array is (array[0]) : xxxxxxxxxx
- The size of an integer (number of bytes) is : xxxxxxxxx
- The size of the whole array is : xxxxxxxxx
- End the program by printing out the array, then double all values in the array by use of a pointer and
- print it out again.
- */
 
+ File name: exerc_2_5.c (or cpp)
+
+ Date: 2017-01-23
+
+ Group Number:  #nr 4
+
+ Members of students contributed:
+
+ Rafael Antonino Sauleo
+
+ Filip Isakovski
+
+ Maria-Bianca Cindroi
+
+ Demonstration code: [<Examen code> <xxxx>] PENDING
+
+You should develop a program that calculates some statistical values for an array of integers. Among
+other things, the program will plot a histogram for the frequency of the different input numbers in
+the array.
+To test it you need first to create an array of integers (table [MAX]) with MAX number of random
+numbers between 0 and MAXNUMBER. Then you should write a function that for each possible
+number between 0 – MAXNUMBER calculates how many times the number exists in the array. The
+result is then stored in a new array (frequency []).
+Finally, you write a function that given the array frequency [] draw a histogram as followed example:
+You should use the function and its function declaration below.
+Given an array table[]={ 1,2,12, 5,1,0,0,5,9,12, 0,2,3,0} thhe program will printout:
+0 xxxx
+1 xx
+2 xx
+3 x
+5 xx
+9 x
+12 xx
+Note: Numbers with frequency 0 in the array frequency[] is not printed out
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
-#define MAX 10 //define the size of the array
+#define MAX 100
+#define MAXNUMBER 20
 
+void create_random( int tab[]) {
+    srand(time(NULL)); //initialize the random number generator.
+    for (int i = 0; i < MAX; i++){
+        tab[i] = (rand() % (MAXNUMBER +1)); //it will reset the random generator when time changes. (numbers from 0 to MAXNUMBER)
+    }
+}
+
+void count_frequency(int tab[], int freq[]){
+
+    for(int i=0;i<MAX;i++){
+        int count=1;
+        for(int j=i+1;j<= MAX-1; j++){
+            if(tab[i]==tab[j] && tab[i]!='\0'){
+                count++;
+                tab[j]='\0'; //0 and \0 is the same
+            }
+        }
+        if(tab[i]!='\0'){
+            freq[i] = count;
+        }
+    }
+}
+
+
+
+void bubbleSort(int tab[], int array_size) { //sorting method
+    int temp;
+    for(int i = 0; i < MAX; i++)
+    {
+        for(int j = 1; j < MAX-1; j++)
+        {
+            if(tab[j] < tab[i])
+            {
+                //swap them
+                temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
+        }
+    }
+
+}
+
+
+void draw_histogram(int freq[], int tab[]){
+    char stringX = 'x';
+    for(int i=0;i<MAX;i++){
+        if(tab[i]!='\0'){
+            printf("%d is %d times.\n",tab[i],freq[i]);
+        }
+    }
+}
 
 int main(int argc, char **argv) {
-    int array[MAX]; //fixed size of an array of integers with value MAX
-    srand(time(NULL)); //initialize the random number generator.
-    int *labelPtr; //assign pointers this case is an int pointer (*'s).
-    labelPtr = &array; // store address of var in pointer variable
-    int i;
-    for (i = 0; i < MAX; i++){
-        array[i] = (rand() % 101); //it will reset the random generator when time changes. (numbers from 0 to 100)
-    }
-    printf("The value of the label array (address) is: %x %x\n", labelPtr, &array); //allocate memory address
-    printf("First integer in the array is: %d\n", array[0]);
-    printf("The size of an integer (number of bytes) is: %ld \n", sizeof(array[0])); //FIX
-    printf("The size of the whole array is : %d\n", MAX);
-    printf("The size of the whole array is :");
+    int table[MAX]; //fixed size of an array of integers with value MAX
+    int frequency[MAX] = {0};
+    create_random(table);
 
-    for (i = 0; i < MAX; i++){
-        printf("%d ", array[i]);
+    for (int i = 0; i < MAX; i++){      //HERE RUN FUNCTION FOR FREQ
+        printf("%d\n", table[i]);
     }
 
-    printf("\nThe size of the whole double array is :");
-    for (i = 0; i < MAX; i++){
-        array[i] = *(labelPtr + i) * 2;
-        printf("%d ", array[i]);
+    printf("new \n");
+
+    /*
+     * FIRST WE SORT THE ARRAY
+     */
+    bubbleSort(table, MAX);
+    for (int i = 0; i < MAX; i++){
+        printf("%d\n", table[i]);
     }
+
+    /*
+     * We run the function to detect duplicates. If they are we count them. Here 0 is ignored
+     */
+
+    count_frequency(table, frequency);
+
+
+    draw_histogram(frequency, table);
+
+
+
     return(0);
 }
