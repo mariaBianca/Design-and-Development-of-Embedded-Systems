d1f4e74e1a7a69037ae122e4a87da296c8994d4e
isofill <filip.saver15@gmail.com>
2017-01-25 14:47:55 +0100

    Update exerc_2_3.c

--- Work_Package2/exerc_2_3.c
+++ Work_Package2/exerc_2_3.c
@@ -23,38 +23,65 @@
 #include <memory.h>
 
 int main(int argc, char *argv[]) {
+	
+	char *string1 = argv[1];
+	char *string2 = argv[2];
+	int res=0, i=0, trigger=0;
+	
+	printf("String 1: %s\n", string1);
 
-    char *string1 = argv[1];
-    char *string2 = argv[2];
-    int res=0;
+	printf("String 2: %s\n", string2);
+	
+	int str1length = 0;
+	int str2length = 0;
+	
+	//Getting the lengths of the strings
+	str1length = strlen(string1);
+	str2length = strlen(string2);
+	
+	//Checking it our way
+	if(str1length > str2length)
+	{
+		printf("Our result: The strings are different!\n");
+	}
+	else if(str1length < str2length)
+	{
+		printf("Our result: The strings are different!\n");
+	}
+	else
+	{
+		while(string1[i] != '\0')
+		{
+			if((string1[i] != string2[i]) && trigger == 0){
+				trigger = 1;
+			}
+			i++;
+		}
+		
+		if(trigger == 1)
+		{
+			printf("Our result: The strings are different!\n");
+		}
+		else
+		{
+			printf("Our result: The strings are identical!\n");
+		}
+	}
 
-    printf("String 1: %s\n", string1);
-
-    printf("String 2: %s\n", string2);
-
-    int str1length = 0;
-    int str2length = 0;
-
-    //Getting the lengths of the strings
-    str1length = strlen(argv[1]);
-    str2length = strlen(argv[2]);
-
-    //Checking with strcmp()
-    res = strcmp(string1, string2);
-
-    //Using result from strcmp(), AND doing another check without it
-    if(res < 0 && str1length < str2length)
-    {
-        printf("String 1 is shorter!");
-    }
-    else if(res > 0 && str1length > str2length)
-    {
-        printf("String 2 is shorter!");
-    }
-    else
-    {
-        printf("Both strings are equal in length!");
-    }
-
-    return 0;
+	//Checking with strcmp()
+	res = strcmp(string1, string2);
+	
+	//Using result from strcmp()
+	if(res == 0)
+	{
+		printf("strcmp result: The strings are identical!\n");
+	}
+	else 
+	{
+		printf("strcmp result: The strings are different!\n");
+	}
+	
+	
+	
+	return 0;
 }
